6e0d25497a64d027e457fc6c8b17e195e59b4556
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2023-12-23 13:51:40 +0530

    23-12-2023

--- c/STRUCTURE/simple.c
+++ c/STRUCTURE/simple.c
@@ -1,13 +1,13 @@
+//hello
+
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    struct info // user defined data type
+    struct info                                             // user defined data type
     {
         int id;
         int marks;
-    };
-
-    struct info student1;     //declare variable student1 of info structure.
+    }student1;                                            // declare variable student1 of info structure.
 
     student1.id = 1;
     student1.marks = 99;
@@ -15,14 +15,13 @@ int main(int argc, char const *argv[])
     printf("student1 id=%d\n", student1.id);
     printf("student1 marks=%d\n", student1.marks);
 
-     struct info student2;     //declare variable student2 of info structure.
+    struct info student2;                                 // declare variable student2 of info structure.
 
     printf("enter the id and marks : ");
-    scanf("%d",&student2.id);
-    scanf("%d",&student2.marks);
+    scanf("%d", &student2.id);
+    scanf("%d", &student2.marks);
     printf("student2 id=%d\n", student2.id);
     printf("student2 marks=%d\n", student2.marks);
-
 
     return 0;
 }
