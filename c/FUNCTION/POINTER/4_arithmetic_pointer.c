6c32693149d8349d0ac1625af0e34f2a0467ed3e
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2023-12-25 14:48:31 +0530

    25-12-2023

---
+++ c/FUNCTION/POINTER/4_arithmetic_pointer.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=2;
+
+    int *x=&a;
+
+    printf("%d\n",a);
+    printf("%d\n",*x);
+    printf("%u\n",&a);
+    printf("%u\n",x);
+
+    x++;
+    printf("\n after x++ \n");
+    printf("%u\n",x);
+
+    //in output we can saw that if we do x++ or x+1 then than output is x + sizeof(int)*1.
+    x-=2;
+    printf("\n\after x-2\n");
+    printf("%u\n",x);
+    return 0;
+}
