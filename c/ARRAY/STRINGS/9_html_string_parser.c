e40d2ec79339beb3b982ce485ff41a6a047434eb
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2023-12-26 22:47:11 +0530

    26-12-2023

---
+++ c/ARRAY/STRINGS/9_html_string_parser.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+
+void parser(char string[])
+{
+    int in=0;  //variable to track that e are in tag or not. tage means ex., <h1>
+    int index=0;
+
+    for(int i=0;string[i]!='\0';i++)
+    {
+        if(string[i]=='<')
+        {
+            in=1;
+            continue;
+        }
+        else if(string[i]=='>')
+        {
+            in=0;
+            continue;
+        }
+
+        if(in==0)
+        {
+            string[index]=string[i];
+            index++;
+            
+        }
+        //string[i]='\0';   
+    }
+    string[index]='\0';  //this is very important.
+
+}
+
+void spaceremove(char string[])
+{   
+    int i;
+    int index=0;
+    for(i=0;string[i]==' ';i++);
+
+    for(;string[i]!='\0';i++)
+    {
+        string[index]=string[i];
+        index++;
+        //string[i]='\0';
+    }
+    string[index]='\0';
+
+    for(i=strlen(string)-1;string[i]==' ';i--);
+
+    string[i+1]='\0';
+
+    
+}
+int main(int argc, char const *argv[])
+{
+    char string[] = "<h1>     this is heading   </h1>";
+
+    printf("before parser : ");
+    puts(string);
+    printf("\n\n");
+
+    parser(string);
+    spaceremove(string);
+
+    printf("after parsed string is ~~%s~~\n\n",string);
+
+    return 0;
+}
