cdf2d01684ae98a876d0031fe7fd52e0f48f9da1
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-01-01 11:34:22 +0530

    01-01-2024

---
+++ c/SIMPLE/simple.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=2;
+
+    printf("%d\n",a);
+    printf("%d\n",&a);
+    printf("%p\n",&a);
+    printf("%d\n",(6422300));
+    return 0;
+}
