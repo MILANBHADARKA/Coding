ca52d257b83adb016d680262ce19cb056dd198f7
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-01-01 12:46:08 +0530

    1-1-2024

--- c/SIMPLE/simple.c
+++ c/SIMPLE/simple.c
@@ -1,11 +1,37 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int a=2;
+    // int a=2;
 
-    printf("%d\n",a);
-    printf("%d\n",&a);
-    printf("%p\n",&a);
-    printf("%d\n",(6422300));
-    return 0;
+    // printf("%d\n",a);
+    // printf("%d\n",&a);
+    // printf("%p\n",&a);
+    // printf("%u\n",&a);
+    // return 0;
+
+    char *str[3]={"abc","def","ghi"};
+
+    for(int i=0;i<3;i++)
+    {
+        puts(str[i]);
+        //printf("%s\n",str[i]);
+    }
+    printf("\n");
+
+
+    str[1]="new";
+
+    for(int i=0;i<3;i++)
+    {
+        puts(str[i]);
+        //printf("%s\n",str[i]);
+    }
+
+    char arr[][5]={"jkl","mno","pqr"};
+
+    puts(arr[1]);
+
+
+
+
 }
